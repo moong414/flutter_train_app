85c1e58fc52edb2351c3615a836d84274aa3c333
moong414 <moongom414@gmail.com>
2025-07-14 14:42:33 +0900

    fix: reserve_page Stateless위젯으로 변경

--- lib/reserve_page.dart
+++ lib/reserve_page.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_train_app/util.dart';
 
-class ReservePage extends StatefulWidget {
+class ReservePage extends StatelessWidget {
   const ReservePage({super.key, required this.selectedStationMap});
   final Map<int, String> selectedStationMap;
 
-  @override
-  State<ReservePage> createState() => _ReservePageState();
-}
-
-class _ReservePageState extends State<ReservePage> {
   //예약박스 함수
-
-  //예약박스 UI
   Padding reservedBox(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
@@ -44,29 +37,7 @@ class _ReservePageState extends State<ReservePage> {
                 ],
               ),
               SizedBox(height: 12),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondaryContainer,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Container(
-                        color: Theme.of(context).colorScheme.tertiary,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
-                          child: Text('좌석정보'),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text('1-1', style: TextStyle(fontSize: 14)),
-                    ],
-                  ),
-                ),
-              ),
+              reservedSeatInfo(context),
             ],
           ),
         ),
@@ -74,7 +45,34 @@ class _ReservePageState extends State<ReservePage> {
     );
   }
 
-  //예약된 역 정보 UI
+  //예약된 좌석: 좌석 정보 UI
+  Container reservedSeatInfo(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondaryContainer,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Container(
+              color: Theme.of(context).colorScheme.tertiary,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                child: Text('좌석정보'),
+              ),
+            ),
+            SizedBox(width: 10),
+            Text('1-1', style: TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //예약된 좌석: 역 정보 UI
   Expanded reservedStationInfo() {
     return Expanded(
       child: Row(
@@ -91,7 +89,7 @@ class _ReservePageState extends State<ReservePage> {
   }
 
   //메인페이지로 이동 함수
-  void toMainBtn() {
+  void toMainBtn(BuildContext context) {
     int count = 0;
     Navigator.popUntil(context, (_) => count++ == 3);
   }
@@ -106,7 +104,12 @@ class _ReservePageState extends State<ReservePage> {
       bottomNavigationBar: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
-          child: purpleMainButton(text: '메인으로 이동', onPressed: toMainBtn),
+          child: purpleMainButton(
+            text: '메인으로 이동',
+            onPressed: () {
+              return toMainBtn(context);
+            },
+          ),
         ),
       ),
     );
